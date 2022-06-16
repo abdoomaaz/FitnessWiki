83a7b021ac377161c0c0927c893e0fe9929924f8
abdoomaaz <abodeh98@gmail.com>
2022-06-16 18:39:39 +0200

    Initial Commit

---
+++ FitnessWiki/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FitnessWiki
+//
+//  Created by AbdooMaaz's playground on 16.06.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
